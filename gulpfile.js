e13aed980d54b988771b001d5ab52e70a4a241cf
CyberLess <47046834+CyberLess@users.noreply.github.com>
2020-01-17 13:49:02 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const projectName = __dirname.replace(/.*\\([^\\]+)\\/gm, '').toLowerCase();
+const projectName = 'veramed';//__dirname.replace(/.*\\([^\\]+)\\/gm, '').toLowerCase();
 const path = require('./path.json');
 
 const ftpData = require('./ftp.json');
